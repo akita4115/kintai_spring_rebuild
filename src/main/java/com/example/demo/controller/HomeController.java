54d38beea94f478d9e329e73b7744c165bdec743
Mami Akita <mami.akita@acrocrane.com>
2026-08-12 15:17:07 +0900

    ログイン画面と権限別遷移を移行

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	/**
+	 * トップページ
+	 */
+	@GetMapping("/")
+	public String getIndex() {
+		return "index";
+	}
+
+	/**
+	 * 勤怠入力画面
+	 */
+	@GetMapping("/attendance/input")
+	public String getAttendanceInput() {
+		return "attendance/input";
+	}
+
+	/**
+	 * 勤怠管理画面
+	 */
+	@GetMapping("/attendance/manage")
+	public String getAttendanceManage() {
+		return "attendance/manage";
+	}
+}
